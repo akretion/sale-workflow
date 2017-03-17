f6c61ac45f4b169d7236c96100a1520400e39352
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-17 12:05:19 +0100

    [MIG] sale_automatic_workflow_exception: Migrated to 10.0

--- sale_automatic_workflow_exception/automatic_workflow_job.py
+++ sale_automatic_workflow_exception/automatic_workflow_job.py
@@ -3,7 +3,7 @@
 # Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models
+from odoo import api, models
 
 
 class AutomaticWorkflowJob(models.Model):
