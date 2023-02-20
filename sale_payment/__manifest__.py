a3811ab8734b7d913ef74d434de262c43f1fd7e9
Florian da Costa <florian.dacosta@akretion.com>
2023-02-20 13:38:05 +0100

    Adapt sale order link to new reconciliation widget module

--- sale_payment/__manifest__.py
+++ sale_payment/__manifest__.py
@@ -6,12 +6,12 @@
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
-    "depends": ["sale"],
+    "depends": ["sale", "account_reconcile_oca", "sale_commercial_partner"],
     "data": [
         "security/ir.model.access.csv",
         "views/sale_order_view.xml",
         "wizards/sale_payment_register_views.xml",
-        #        "views/account_bank_statment_views.xml",
+        "views/account_bank_statment_views.xml",
     ],
     "installable": True,
 }
