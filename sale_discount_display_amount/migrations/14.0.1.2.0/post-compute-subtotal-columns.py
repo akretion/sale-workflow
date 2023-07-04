7d61a34293a797099b75b8c062f04f3b23d2f448
Florian Mounier <paradoxxx.zero@gmail.com>
2023-07-04 14:14:03 +0200

    [IMP] sale_discount_display_amount: Delayed migration

--- sale_discount_display_amount/migrations/14.0.1.2.0/post-compute-subtotal-columns.py
+++ sale_discount_display_amount/migrations/14.0.1.2.0/post-compute-subtotal-columns.py
@@ -32,5 +32,19 @@ def migrate(cr, version):
     cr.execute(query)
     order_ids = cr.fetchall()
 
-    orders = env["sale.order"].search([("id", "in", order_ids)])
-    orders.mapped("order_line")._update_discount_display_fields()
+    so = env["sale.order"].search([("id", "in", order_ids)])
+    count = len(so)
+    if count == 0:
+        _logger.info("No sale orders to update found.")
+        return
+    _logger.info("Scheduling without discount prices update of %d orders", count)
+
+    batch_size = 250
+    for part in range(1 + count // batch_size):
+        start = part * batch_size
+        end = min((part + 1) * batch_size, count)
+        if end > start:
+            so[start:end].mapped("order_line").with_delay(
+                description="Without discount price update batch %d-%d / %d"
+                % (start + 1, end, count)
+            )._update_discount_display_fields()
