43e26b7d35663e1ebc75bf9952531c5976cde7af
Francois Poizat <francois.poizat@gmail.com>
2025-08-27 16:17:16 +0200

    [IMPL] remove sale_price_config from module

--- sale_mrp_bom_configurable/models/stock_lot.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class StockLot(models.Model):
-    _name = "stock.lot"
-    _inherit = "stock.lot"
-
-    input_line_id = fields.Many2one(comodel_name="input.line")
--- sale_mrp_bom_configurable/wizard/__init__.py
+++ sale_mrp_bom_configurable/wizard/__init__.py
@@ -1,3 +1,1 @@
-from . import matrix_wizard
 from . import wizard_copy_input_line_data
-from . import wizard_sale_price_change
