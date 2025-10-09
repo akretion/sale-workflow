8796029a1ad2a25ae8cd82234ef3508219f6283a
Francois Poizat <francois.poizat@gmail.com>
2025-10-09 11:10:22 +0200

    [IMPL] do not allow change in config after the sale is confirmed

--- sale_mrp_bom_configurable/models/input_line.py
+++ sale_mrp_bom_configurable/models/input_line.py
@@ -1,7 +1,17 @@
 from odoo import fields, models
+from odoo.exceptions import UserError
 
 
 class InputLine(models.Model):
     _inherit = "input.line"
 
     order_line_id = fields.Many2one(comodel_name="sale.order.line", ondelete="cascade")
+
+    def write(self, vals):
+        for rec in self:
+            if rec.order_line_id.order_id.state == "sale":
+                for field_name in rec._get_config_elements():
+                    if field_name in vals:
+                        raise UserError("Can't change config when sale in confirmed")
+        res = super().write(vals)
+        return res
